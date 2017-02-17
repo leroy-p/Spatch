b9f4cf455be82ee237c0af5404c619c373511a70
BEDIDI Fawzi <fawzi.bedidi@epitech.eu>
2017-02-17 13:54:24 +0100

    libs added

--- main.cpp
+++ main.cpp
@@ -12,16 +12,16 @@
 int main() {
 
     User *user = new User("127.0.0.2", "toto");
-  if (user->isAutorized("134.22.55.6") == true)
-    std::cout << "Vous avez acces au serveur 134.22.55.6" << std::endl;
-  else
-    std::cout << "Vous n'avez pas acces au serveur 134.22.55.6" << std::endl;
+    if (user->isAutorized("134.22.55.6") == true)
+        std::cout << "Vous avez acces au serveur 134.22.55.6" << std::endl;
+    else
+        std::cout << "Vous n'avez pas acces au serveur 134.22.55.6" << std::endl;
 
-  if (user->isAutorized("123.23.12.1")== true)
-    std::cout << "Vous avez acces au serveur 123.23.12.1" << std::endl;
-  else
-    std::cout << "Vous n'avez pas acces au serveur 123.23.12.1" << std::endl;
- init_spatch();
+    if (user->isAutorized("123.23.12.1") == true)
+        std::cout << "Vous avez acces au serveur 123.23.12.1" << std::endl;
+    else
+        std::cout << "Vous n'avez pas acces au serveur 123.23.12.1" << std::endl;
+    init_spatch();
 
     return 0;
 }
